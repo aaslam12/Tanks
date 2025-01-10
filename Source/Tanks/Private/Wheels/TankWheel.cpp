6cdbbc753e88e2063f5c680bdd668a4118794df9
AL <alturoxx@gmail.com>
2025-01-09 21:46:17 -0700

    Got the tank controls to be much better now.

--- Source/Tanks/Private/Wheels/TankWheel.cpp
+++ Source/Tanks/Private/Wheels/TankWheel.cpp
@@ -5,34 +5,4 @@
 
 UTankWheel::UTankWheel()
 {
-	///////////////////////////////////////////////////
-	/// Wheel
-	Offset = FVector(0, 0, -15);
-	WheelRadius = 32.0;
-	WheelWidth = 40.0;
-	WheelMass = 500.0;
-	CorneringStiffness = 1000.0;
-	SideSlipModifier = 1.0;
-	SlipThreshold = 0.0;
-	SkidThreshold = 0.0;
-	bAffectedByBrake = true;
-	bAffectedByHandbrake = true;
-	bAffectedByEngine = true;
-	bABSEnabled = true;
-	
-	///////////////////////////////////////////////////
-	/// Suspension
-	SuspensionMaxRaise = 10.0;
-	SuspensionMaxDrop = 10.0;
-	SuspensionDampingRatio = 0.5;
-	WheelLoadRatio = 0.5;
-	SpringRate = 250.0;
-	SuspensionSmoothing = 5;
-	RollbarScaling = 0.15;
-	SweepShape = ESweepShape::Spherecast;
-
-	///////////////////////////////////////////////////
-	/// Brakes
-	MaxBrakeTorque = 1500.0;
-	MaxHandBrakeTorque = 4000.0;
 }
