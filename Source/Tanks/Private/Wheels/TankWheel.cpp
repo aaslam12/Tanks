cffcbbd422b862e7e46830d77dbaa5a7ef80b59f
AL <alturoxx@gmail.com>
2025-01-11 22:22:23 -0700

    Added code that changes the trace type for wheels depending on the scalability settings.

--- Source/Tanks/Private/Wheels/TankWheel.cpp
+++ Source/Tanks/Private/Wheels/TankWheel.cpp
@@ -3,6 +3,45 @@
 
 #include "Tanks/Public/Wheels/TankWheel.h"
 
+#include "GameFramework/GameUserSettings.h"
+
 UTankWheel::UTankWheel()
 {
+	auto UserSettings = UGameUserSettings::GetGameUserSettings();
+
+	if (!UserSettings)
+		return;
+
+	// Value -1:custom, 0:low, 1:medium, 2:high, 3:epic, 4:cinematic
+	auto ScalabilityLevel = UserSettings->GetOverallScalabilityLevel();
+
+	switch (ScalabilityLevel)
+	{
+		case -1:
+		{
+			SweepShape = ESweepShape::Spherecast;
+			break;
+		}
+		
+		case 0:
+		case 1:
+		{
+			SweepShape = ESweepShape::Raycast;
+			break;
+		}
+
+		case 2:
+		case 3:
+		{
+			SweepShape = ESweepShape::Spherecast;
+			break;
+		}
+		
+		case 4:
+		{
+			SweepShape = ESweepShape::Shapecast;
+			break;
+		}
+	default: break;
+	}
 }
