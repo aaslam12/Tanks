01d7c8dec335f594d2b15bba02b5d0065a57bb6f
AL <alturoxx@gmail.com>
2025-05-18 16:36:37 -0700

    Add collision profiles and implement MinePowerUp class
    Defined new collision profiles in the DefaultEngine.ini.
    
    Added the AMinePowerUp class.

---
+++ Source/Tanks/Public/Actors/MinePowerUp.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PowerUp.h"
+#include "MinePowerUp.generated.h"
+
+UCLASS(Abstract)
+class TANKS_API AMinePowerUp : public APowerUp
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMinePowerUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
